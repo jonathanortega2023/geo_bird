c5871b659321722b637868650c3fb25eecb3e8fa
Jon <jonathanortega2023@gmail.com>
2023-02-04 03:27:24 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/geo_bird/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.geo_bird
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
+import './services/geo.dart';
+
+List<CameraDescription> _cameras = <CameraDescription>[];
+Future<void> main() async {
+  // Fetch the available cameras before initializing the app.
+  try {
+    _cameras = await availableCameras();
+    runApp(MaterialApp(home: PhotoPage()));
+  } on CameraException catch (e) {
+    _logError(e.code, e.description);
+    runApp(MaterialApp(home: CameraErrorPage(cameraException: e)));
+  }
+}
+
+class CameraErrorPage extends StatelessWidget {
+  final CameraException cameraException;
+  const CameraErrorPage({required this.cameraException, super.key});
+  @override
+  Widget build(BuildContext context) {
+    if (cameraException.description == null) {
+      return Scaffold(
+        body: Center(child: Text(cameraException.code)),
+      );
+    } else {
+      return Scaffold(
+        body: Center(
+            child: Text(
+                "${cameraException.code}: ${cameraException.description}")),
+      );
+    }
+  }
+}
+
+void _logError(String code, String? message) {
+  // ignore: avoid_print
+  print('Error: $code${message == null ? '' : '\nError Message: $message'}');
+}
+
+class PhotoPage extends StatefulWidget {
+  const PhotoPage({super.key});
+
+  @override
+  State<PhotoPage> createState() => _PhotoPageState();
+}
+
+class _PhotoPageState extends State<PhotoPage> {
+  CameraController? controller =
+      CameraController(_cameras.first, ResolutionPreset.max);
+  XFile? imageFile;
+  Position? geoPosition;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: _capturePhotoWidget());
+  }
+
+  Widget _capturePhotoWidget() {
+    final CameraController? cameraController = controller;
+
+    return Scaffold(
+      body: Center(
+        child: IconButton(
+          icon: const Icon(Icons.camera_outlined),
+          onPressed: onTakePictureButtonPressed,
+        ),
+      ),
+    );
+  }
+
+  Future<void> onTakePictureButtonPressed() async {
+    try {
+      await controller?.initialize();
+    } on CameraException catch (e) {
+      _handleFailedCameraInit(e);
+    }
+    takePicture().then((XFile? file) {
+      try {
+        determinePosition().then((Position? position) {
+          setState(() {
+            geoPosition = position;
+          });
+          print('''
+                Lat: ${geoPosition.toString()}
+                Accuracy: ${geoPosition!.accuracy}
+                GeoTimestamp: ${geoPosition!.timestamp}
+                ''');
+        });
+      } on GeolocatorException catch (e) {
+        _logError(e.code, e.message);
+        showInSnackBar(e.message);
+      } on Exception catch (e) {
+        print(e.toString());
+        showInSnackBar(e.toString());
+      }
+      if (mounted && geoPosition != null) {
+        setState(() {
+          imageFile = file;
+        });
+        if (file != null) {
+          // TODO Confirmation screen
+          // TODO Upload to database
+          showInSnackBar(
+              'Picture saved to ${file.path} at ${geoPosition!.toString()} on ${geoPosition!.timestamp}');
+        }
+      }
+    });
+  }
+
+  Future<XFile?> takePicture() async {
+    final CameraController? cameraController = controller;
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      showInSnackBar('Error: select a camera first.');
+      return null;
+    }
+
+    if (cameraController.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+
+    try {
+      final XFile file = await cameraController.takePicture();
+      return file;
+    } on CameraException catch (e) {
+      _logError(e.code, e.description);
+      return null;
+    }
+  }
+
+  String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
+
+  void _showCameraException(CameraException e) {
+    _logError(e.code, e.description);
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+
+  void showInSnackBar(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _handleFailedCameraInit(CameraException e) {
+    switch (e.code) {
+      case 'CameraAccessDenied':
+        showInSnackBar('You have denied camera access.');
+        break;
+      case 'CameraAccessDeniedWithoutPrompt':
+        // iOS only
+        showInSnackBar('Please go to Settings app to enable camera access.');
+        break;
+      case 'CameraAccessRestricted':
+        // iOS only
+        showInSnackBar('Camera access is restricted.');
+        break;
+      default:
+        _showCameraException(e);
+        break;
+    }
+  }
+}
